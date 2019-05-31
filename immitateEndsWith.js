ecafc93fd7daac05ccd8f9983fba453687f464b3
Daryna Hunko <18738469+dhunko@users.noreply.github.com>
2019-05-31 15:56:14 +0300

    fix

--- immitateEndsWith.js
+++ immitateEndsWith.js
@@ -1,11 +1,12 @@
 function immitateEndsWith(str, substr, position = 0) {
     substr = substr.split("").reverse().join("");
     str = str.split("").reverse().join("");
-    if (position != 0) {
-        position = str.length - position - 1;
-    }
-    if ((str.includes(substr)) && (str.charAt(position) === substr.charAt(0))) {
-        return true;
+    if ((str.includes(substr))) {
+        substr = substr.split("").reverse().join("");
+        str = str.split("").reverse().join("");
+        if (str.charAt(position) === substr.charAt(0)) {
+            return true;
+        }
     }
     return false;
 }
@@ -14,3 +15,4 @@ console.log(immitateEndsWith("hello guys", "uys"));
 console.log(immitateEndsWith("hello guys", "uhys"));
 console.log(immitateEndsWith("hello guys", "ll", 2));
 console.log(immitateEndsWith("hello guys", "ll", 1));
+console.log(immitateEndsWith("hellfdfdfdfdo guys", "dfdfdf", 5));
