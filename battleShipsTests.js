9a9b67a19bd7571575a3d9da7a2b4f3c126c4c46
Daryna Hunko <18738469+dhunko@users.noreply.github.com>
2019-05-31 12:28:34 +0300

    Update battleShipsTests.js

--- battleShipsTests.js
+++ battleShipsTests.js
@@ -2,7 +2,7 @@ describe("damagedOrSunk", function() {
 
   it("возвращает баллы, сколько сбитых, ранненых, и нетронутых кораблей", function() {
     assert.equal(damagedOrSunk([[0,0,0,2,2,0],[0,3,0,0,0,0],[0,3,0,1,0,0],[0,3,0,1,0,0]], [[2, 1], [1, 3], [4, 2]]),
-                 '{ sunk: ' + 0 + ', damaged: ' + 2 + ', notTouched: ' + 1 + ', points: ' + 0,5 + '}');
+                 '{ sunk: 0, damaged: 2, notTouched: 1, points: 0 }');
   });
 
 });
