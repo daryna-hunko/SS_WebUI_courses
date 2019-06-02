71abff6e5b03ae7bc3ae98fcfcb175a97ae19325
Daryna Hunko <18738469+dhunko@users.noreply.github.com>
2019-06-02 11:27:34 +0300

    added ToUpperCase & ToLowerCase functions

---
+++ immitateStringMethods/immitateToUpperORLowerCase.js
@@ -0,0 +1,45 @@
+function immitateToUpperCase(string) {
+    if (typeof(string) !== 'string') {
+        return 'undefined';
+    }
+    string = string.split('');
+        console.log(string);
+    let newStr = [];
+    let charArr = [];
+    for (var index in string) {
+        let charNum = string[index].charCodeAt(0);
+        if (96 < charNum && charNum < 123) {
+            charNum -= 32;
+        }
+        charArr.push(charNum);
+    }
+    for (var index in charArr) {
+      let text = String.fromCharCode(charArr[index]);
+      newStr.push(text);
+    }
+    return newStr.join('');
+}
+console.log(immitateToUpperCase('tEst6'));
+
+function immitateToLowerCase(string) {
+    if (typeof(string) !== 'string') {
+        return 'undefined';
+    }
+    string = string.split('');
+        console.log(string);
+    let newStr = [];
+    let charArr = [];
+    for (var index in string) {
+        let charNum = string[index].charCodeAt(0);
+        if (64 < charNum && charNum < 91) {
+            charNum += 32;
+        }
+        charArr.push(charNum);
+    }
+    for (var index in charArr) {
+      let text = String.fromCharCode(charArr[index]);
+      newStr.push(text);
+    }
+    return newStr.join('');
+}
+console.log(immitateToLowerCase('tEst6'));
