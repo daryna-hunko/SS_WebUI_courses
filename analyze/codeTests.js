e81a5fb9a5b090d4ee68bbe52152d55b394a609f
dhunko <18738469+dhunko@users.noreply.github.com>
2019-06-04 23:07:35 +0300

    day 10

--- analyze/codeTests.js
+++ analyze/codeTests.js
@@ -2,7 +2,6 @@ describe("toData", function() {
 
     it("returns the decimal number", function() {
         assert.equal(NumC.toData(456454), 456454);
-        assert.equal(NumC.toData(' fg  ggfb1313'), NaN);
         assert.equal(NumC.toData(' 1313 &nbsp;'), 1313);
     });
 
@@ -17,13 +16,12 @@ describe("isNumeric", function() {
     it("check whether an input is a number", function() {
         assert.equal(NumC.isNumeric(456454), true);
         assert.equal(NumC.isNumeric(' fg  ggfb1313'), false);
-        assert.equal(NumC.toData(), false);
     });
 
     it("check whether an input is a number typeOf check", function() {
-        assert.typeOf(NumC.toData(456454), 'boolean');
+        assert.isTrue(NumC.isNumeric('456454,24'));
     });
-  
+
   });
 
 describe("toSymbol", function() {
@@ -38,7 +36,7 @@ describe("toSymbol", function() {
 describe("toCurrency", function() {
 
     it("returns strings with currency abbr by the input with the currency symbol", function() {
-        assert.equal(NumC.toSymbol("$"), "USD");
+        assert.equal(NumC.toSymbol("₴"), "UAH");
         assert.typeOf(NumC.toSymbol("$"), 'string');
     });
   
