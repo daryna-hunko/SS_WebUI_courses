8b42bc9a07e574925c7170090c4c936e923681c9
dhunko <18738469+dhunko@users.noreply.github.com>
2019-06-17 21:33:02 +0300

    fixed demo tests

--- Demo_tasks/Demo2/demo_2_tests.js
+++ Demo_tasks/Demo2/demo_2_tests.js
@@ -1,10 +1,10 @@
 describe("envelopes", function() {
 
-  it("returns the envelope that is bigger", function() {
+  it("returns the envelope that can be included", function() {
       expect(envelopes([2,2], [2,4])).to.eql([2,2]);
   });
 
-  it("returns the envelope that is bigger", function() {
+  it("returns the envelope that can be included", function() {
       expect(envelopes([12,2], [2,4])).to.eql([2,4]);
   });
 
